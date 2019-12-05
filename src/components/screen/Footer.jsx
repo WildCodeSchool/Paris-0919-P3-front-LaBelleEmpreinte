5b8b4c24cd5f8741697a54c6731fcd5b2a300c7e
Tass2T <brunog90@gmail.com>
2019-12-05 10:34:48 +0100

    bootstrap added

---
+++ src/components/screen/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import baniere from '../../assets/pictures/belleempreinte.png'
+import logo from '../../assets/icons/labelleempreinte-green.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <img src={baniere} alt="banniere"></img>
+            <div className="encadre">
+                <img src={logo} alt="logo"></img>
+                <p>Un site réalisé par une belle équipe indépendante basée à Paris. Merci beaucoup à tous ceux qui ont aidé à la réalisation de ces outils qui n’existeraient pas sans eux </p>
+                <div className="list">
+                    <ul>
+                        <li>CONTACT: delphine@labelleempreinte.fr</li>
+                        <li>Mentions légales</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
