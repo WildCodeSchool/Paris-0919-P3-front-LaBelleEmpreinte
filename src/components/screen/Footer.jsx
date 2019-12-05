af7516c0af0ad92aa48219e43416fcf3aa32c070
Tass2T <brunog90@gmail.com>
2019-12-05 12:02:53 +0100

    grid added

--- src/components/screen/Footer.jsx
+++ src/components/screen/Footer.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import './CSS/Footer.css'
+
+// images utilisées pour ce footer
 import baniere from '../../assets/pictures/belleempreinte.png'
 import logo from '../../assets/icons/labelleempreinte-green.png'
+
+// bootstrap pour le button
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 export default function Footer() {
     return (
         <div className="footer">
             <img className="banniere" src={baniere} alt="banniere"></img>
             <div className="encadre">
-                <img src={logo} alt="logo" style={{ width: 128}}></img>
-                <p>Un site réalisé par une belle équipe indépendante basée à Paris. Merci beaucoup à tous ceux qui ont aidé à la réalisation de ces outils qui n’existeraient pas sans eux </p>
+                <img className="logo" src={logo} alt="logo" style={{ width: 128}}></img>
+                <p className="remerciements">Un site réalisé par une belle équipe indépendante basée à Paris. Merci beaucoup à tous ceux qui ont aidé à la réalisation de ces outils qui n’existeraient pas sans eux </p>
                 <div className="list">
                     <ul>
-                        <li>CONTACT: delphine@labelleempreinte.fr</li>
-                        <li>Mentions légales</li>
+                        <li><b>CONTACT:</b> delphine@labelleempreinte.fr</li>
+                        <li><b>Mentions légales</b></li>
                     </ul>
                 </div>
+                <Button className="button" variant="success" size="m">La lettre des emplettes responsables</Button>
             </div>
         </div>
     )
