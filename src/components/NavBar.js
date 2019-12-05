ee59262eef190d1dca93d520cd01802b87f48db0
adamsbah <ballyadamsco@gmail.com>
2019-12-06 00:22:52 +0100

    navbar react

---
+++ src/components/NavBar.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+ 
+import "./NavBar.css"
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div>
+                    <div className="img-logo">
+                    <a className="navbar-brand" href="/">
+                        <img src="/img/labelleempreinte-green.png" alt="la_belle_empreinte"  widht="130"/>
+                    </a>
+                    </div>
+                    <ul className="sidebar">
+                        <li className="nav-item">
+                         <a className="nav-link active" href="/homepage">
+                          ACCUEIL
+                          
+                         </a>
+                         </li>
+                    <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div>
+                    <li className="nav-item">
+                     <a className="nav-link" href="/quizz">
+                     QUIZZ & GUIDE
+                     </a>
+                    </li> 
+                    <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div> 
+                     <li className="nav-item">
+                      <a className="nav-link" href="/presentation">
+                      PRESENTATION
+                      </a>
+                     </li> 
+                     <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div> 
+                     <li className="nav-item">
+                      <a className="nav-link after-red" href="/moteur-de-recherche">
+                      MOTEUR DE RECHERCHE
+                      </a>
+                     </li>  
+                     <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div> 
+                     <li className="nav-item">
+                      <a className="nav-link after-orange" href="/label">
+                      LABEL
+                      </a>
+                     </li>
+                     <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div> 
+                     <li className="nav-item">
+                      <a className="nav-link" href="/nous rejoindre">
+                      NOUS REJOINDRE
+                      </a>
+                     </li> 
+                     <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div> 
+                     <li className="nav-item">
+                      <a className="nav-link" href="/blog">
+                      BLOG
+                      </a>
+                     </li>  
+                     <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div> 
+                     <li className="nav-item">
+                      <a className="nav-link" href="/premier pas">
+                      PREMIER PAS
+                      </a>
+                     </li> 
+                     <div className="row">
+                     <div className="col-9 offset-3">
+                      <hr className="my-1" />
+                     </div>
+                    </div> 
+                     <li className="nav-item">
+                      <a className="nav-link green" href="coin-des-pros">
+                      LE COIN DES PROS
+                      </a>
+                     </li> 
+                    </ul>
+                    <div className="row my-1 row-newsletter">
+                     <div className="col-9 offset-3 text-center">
+                      <button className="btn-green" data-toggle="modal" 
+                      data-target="#newsletterModal">
+                       <i className="fa fa-envelope"></i>
+                       La lettre des emplettes responsables
+                       </button>
+                     </div>
+                    </div> 
+                    
+                       
+                </div>       
+            
+                
+            </div>
+        )
+    }
+}
+export default  NavBar 
