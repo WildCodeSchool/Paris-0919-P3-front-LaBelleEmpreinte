7343d5e87bfdbc3241e8a5c83c1bce1976d09415
Tass2T <brunog90@gmail.com>
2019-12-05 10:56:17 +0100

    css almost done

--- src/components/screen/Footer.jsx
+++ src/components/screen/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './CSS/Footer.css'
 import baniere from '../../assets/pictures/belleempreinte.png'
 import logo from '../../assets/icons/labelleempreinte-green.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,9 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Footer() {
     return (
         <div className="footer">
-            <img src={baniere} alt="banniere"></img>
+            <img className="banniere" src={baniere} alt="banniere"></img>
             <div className="encadre">
-                <img src={logo} alt="logo"></img>
+                <img src={logo} alt="logo" style={{ width: 128}}></img>
                 <p>Un site réalisé par une belle équipe indépendante basée à Paris. Merci beaucoup à tous ceux qui ont aidé à la réalisation de ces outils qui n’existeraient pas sans eux </p>
                 <div className="list">
                     <ul>
