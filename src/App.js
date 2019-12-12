2bdda1be081cf4582ea29b6f180832eaf4530067
Elgur <leroy.maxence@yahoo.com>
2019-12-12 16:49:08 +0100

    redifined components flexboxes of app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/screen/Header'
 import './App.css';
+import Navbar from './components/screen/Navbar';
+import Footer from './components/screen/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navbar />
+      <div className="app_container">
+        <Header/>
+        <Footer />
+      </div>
     </div>
   );
 }
