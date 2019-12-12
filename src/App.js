290c1e38f0182ea55e1015164d3fd1f37f68bc1b
Tass2T <brunog90@gmail.com>
2019-12-12 15:53:36 +0100

    about to combine screen components, header is now responsive with mediaquery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Footer from './components/screen/Footer'
 import Header from './components/screen/Header'
-import Navbar from './components/screen/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Header/>
     </div>
   );
 }
