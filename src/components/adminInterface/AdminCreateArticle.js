739b733d0ffcf4b3e008ae77a193c1d048b661b1
Nicolas Borson <nicolasborson@hotmail.fr>
2020-01-15 18:46:59 +0100

    form created

--- src/components/adminInterface/AdminCreateArticle.js
+++ src/components/adminInterface/AdminCreateArticle.js
@@ -6,6 +6,37 @@ const AdminCreateArticle = () => (
         <h1>
             Créer une article informatif
         </h1>
+        <div id="form-main">
+            <div id="form-div">
+                <form className="form" id="form1">
+                    <p>
+                        <input type="text" className="feedback-input" placeholder="Titre" id="titre" />
+                    </p>
+                    <p>
+                        <input type="text" className="feedback-input" id="auteur" placeholder="Auteur" />
+                    </p>
+                    <p>
+                        <input type="date" className="feedback-input" id="date" placeholder="Date" />
+                    </p>
+                    <p>
+                        <input type="file" className="feedback-input" id="image" placeholder="Image" />
+                    </p>
+                    <p>
+                        <input type="number" className="feedback-input" id="temps_lecture" placeholder="Temps de lecture" />
+                    </p>
+                    <p>
+                        <input type="text" className="feedback-input" id="lieu" placeholder="Lieu" />
+                    </p>
+                    <p>
+                        <textarea className="feedback-input" id="contenu" placeholder="Contenu de l'article"></textarea>
+                    </p>
+                    <div className="submit">
+                        <input type="submit" value="SEND" id="button-blue" />
+                        <div className="ease"></div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 )
 
