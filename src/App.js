8f7ec94a4d25bce1d775946500baf84705ad0160
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-01-17 00:48:57 +0100

    finished mapping all object and besoin categories
    Co-authored-by: paulinedrouhin <pauline.drouhin@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Header from './components/screen/Header';
 import './App.css';
 import Navbar from './components/screen/Navbar';
 import Footer from './components/screen/Footer';
-import Besoins from './components/userInterface/Besoins';
-import Objets from './components/userInterface/Objets';
+import Filtres from './components/userInterface/Filtres';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="app_container">
-      <Besoins/>
-      <Objets />
+  
       <Header/>
+      <div className="categorie">
+      <Filtres />
+      
+      </div>
       <Footer />
       </div>
     </div>
---
+++ src/components/userInterface/Filtres.js
@@ -0,0 +1,226 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import './Filtres.css'
+
+class Objets extends Component {
+  state = {
+    datasObjets: [''],
+
+    isVisibleObjets: false,
+    objetsSelected: {
+      id: 0,
+      name: ""
+    },
+    datasBesoins: [''],
+    isVisibleBesoins: false,
+    besoinsSelected: {
+      id: 0,
+      name: ""
+    }
+    // objetsFormCateg: []
+  };
+
+  componentDidMount() {
+    this.getObjets()
+    this.getBesoins()
+  }
+  
+getObjets = () => {
+    axios
+      .get( 'http://localhost:4000/user/objets')
+      .then(response => response.data)
+      .then(data => {
+        this.setState({
+          datasObjets: data
+        });
+      });
+  };
+
+  getBesoins = () => {
+    axios
+      .get( 'http://localhost:4000/user/besoins')
+      .then(response => response.data)
+      .then(data => {
+        this.setState({
+          datasBesoins: data
+        });
+      });
+  };
+  
+
+  displayObjet = () => {
+    this.setState({ isVisibleObjets: !this.state.isVisibleObjets, isVisibleBesoins: false },
+    );
+  };
+
+  displayBesoins = () => {
+    this.setState({isVisibleBesoins: !this.state.isVisibleBesoins, isVisibleObjets: false})
+}
+
+  render() {
+    console.log('datasObjets', this.state.datasObjets);
+    console.log('besoin selec', this.state.besoinsSelected);
+
+    return (
+      <div className="Filtres_container">
+        <div className="Filtres_title" onClick={this.displayObjet}>Objets</div>
+
+        <div className="Filtres_frame">
+        {this.state.isVisibleObjets
+          ? this.state.datasObjets[0].results.map(categ => {
+              const filterByCategory = this.state.datasObjets[1].results.filter(
+                sousCateg => sousCateg.categories_objets_id === categ.id
+              );
+                
+              return (
+                <div className="Filtres_allCategories">
+                  <p className="Filtres_sousCat" onClick={() =>
+                          this.setState(
+                            {
+                              objetsSelected: {
+                                id: categ.id,
+                                name: categ.categorie
+                              },
+                              isVisibleObjets: !this.state.isVisibleObjets,
+                            }, () => this.state
+                          )
+                        }>{categ.categorie}</p>
+
+                  {filterByCategory.map(sCateg => {
+                    const filterBySousCateg = this.state.datasObjets[2].results.filter(
+                      objet => objet.categories_intermediaires_id === sCateg.id
+              );
+                    return (
+                      <div>
+                    <ul>
+                      <li className="Filtres_catIntermediaires"  key={sCateg.id} onClick={() =>
+                          this.setState(
+                            {
+                              objetsSelected: {
+                                id: sCateg.id,
+                                name: sCateg.name
+                              },
+                              isVisibleObjets: !this.state.isVisibleObjets,
+                            }, () => this.state
+                          )
+                        }
+                      >
+                        {sCateg.name}
+                      </li>
+                    </ul>
+                    {filterBySousCateg.map(objet => {
+                
+                    return (
+                    <ul>
+                      <li className="filtres_objets" key={objet.id} onClick={() =>
+                          this.setState(
+                            {
+                              objetsSelected: {
+                                id: objet.id,
+                                name: objet.name
+                              },
+                              isVisibleObjets: !this.state.isVisibleObjets,
+                            }, () => this.state
+                          )
+                        }
+                      >
+                        {objet.name}
+                      </li>
+                    </ul>)})}
+                    </div>
+
+
+                     ) })} 
+         
+                </div>
+              );
+            })
+          : ""}
+
+        </div>
+
+        <div>
+
+{/* besoins */}
+        <div className="Filtres_title" onClick={this.displayBesoins}>Besoins</div>
+
+<div className="Filtres_frame">
+{this.state.isVisibleBesoins
+  ? this.state.datasBesoins[0].results.map(categ => {
+      const filterByCategory = this.state.datasBesoins[1].results.filter(
+        sousCateg => sousCateg.besoins_id === categ.id
+      );
+        
+      return (
+        <div className="Filtres_allCategories">
+          <p className="Filtres_sousCat" onClick={() =>
+                  this.setState(
+                    {
+                      besoinsSelected: {
+                        id: categ.id,
+                        name: categ.besoins
+                      },
+                      isVisibleBesoins: !this.state.isVisibleBesoins,
+                    }, () => this.state
+                  )
+                }>{categ.besoins}</p>
+
+          {filterByCategory.map(sCateg => {
+            const filterBySousCateg = this.state.datasObjets[2].results.filter(
+              objet => objet.types_activites_id === sCateg.id
+      );
+            return (
+              <div>
+            <ul>
+              <li className="filtres_objets" key={sCateg.id} onClick={() =>
+                  this.setState(
+                    {
+                      besoinsSelected: {
+                        id: sCateg.id,
+                        name: sCateg.types_activites
+                      },
+                      isVisibleBesoins: !this.state.isVisibleBesoins,
+                    }, () => this.state
+                  )
+                }
+              >
+                {sCateg.types_activites}
+              </li>
+            </ul>
+            
+            </div>
+
+
+             ) })} 
+ 
+        </div>
+      );
+    })
+  : ""}
+
+</div>
+
+
+                   
+                   
+            </div>
+        <div>
+          <p>{this.state.objetsSelected.id !== 0
+            ? this.state.objetsSelected.name
+            : ""}</p>
+            <p>{this.state.besoinsSelected.id !== 0
+            ? this.state.besoinsSelected.name
+            : ""}</p>
+
+
+
+          <button onClick={this.getArticlesBySousCategSelected}>
+            Rechercher
+          </button>
+          </div>
+      </div>
+    );
+  }
+}
+export default Objets;
