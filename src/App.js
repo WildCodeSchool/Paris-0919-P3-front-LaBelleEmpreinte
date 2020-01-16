c8065e0c546fcf2d4cd1419d5cfa0ed7deda692a
Tass2T <brunog90@gmail.com>
2020-01-16 15:55:26 +0100

    correction in progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Header from './components/screen/Header'
+// import Header from './components/screen/Header'
 import './App.css';
-import Navbar from './components/screen/Navbar';
-import Footer from './components/screen/Footer';
-import DisplayArticles from './components/screen/DisplayArticles';
-import ArticleContent from './components/screen/ArticleContent';
-import AdminHomeContainer from './store/containers/AdminHomeContainer';
-import AdminCreateArticle from './components/adminInterface/AdminCreateArticle'
-import ListInitiatives from './components/screen/ListInitiatives';
+// import Navbar from './components/screen/Navbar';
+// import Footer from './components/screen/Footer';
+// import DisplayArticles from './components/screen/DisplayArticles';
+// import ArticleContent from './components/screen/ArticleContent';
+// import AdminHomeContainer from './store/containers/AdminHomeContainer';
+// import AdminCreateArticle from './components/adminInterface/AdminCreateArticle'
+// import ListInitiatives from './components/screen/ListInitiatives';
+import Home from './components/adminInterface/Home'
 
 
 
@@ -16,16 +17,7 @@ import ListInitiatives from './components/screen/ListInitiatives';
 const App = () => {
   return (
     <div>
-        {/* <ArticleContent /> */}
-        {/* <DisplayArticles /> */}
-        {/* <div className="App">
-        <Navbar />
-        <div className="app_container">
-          <Header />
-          <Footer />
-        </div> */}
-        <AdminHomeContainer />
-        <AdminCreateArticle />
+        <Home/>
     </div>
   );
 }
