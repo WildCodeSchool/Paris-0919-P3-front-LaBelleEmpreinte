959974fbef0b972746726f0bb23eb0d1d7731fdc
Tass2T <brunog90@gmail.com>
2020-01-16 11:51:31 +0100

    new articleInform has been created

---
+++ src/components/adminInterface/ArticleInform.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function ArticleInform(props) {
+
+
+    return (
+        <div>
+            <h1>Je veux {props.action} {props.category}</h1>
+        </div>
+    )
+}
--- src/components/adminInterface/HomeFilter.jsx
+++ src/components/adminInterface/HomeFilter.jsx
@@ -1,25 +1,48 @@
 import React, { useState, useEffect } from "react"
+import axios from 'axios'
+import ArticleInform from "./ArticleInform"
+
 
 export default function HomeFilter() {
 
-    const [action, setAction] = useState(false)
+    // state that get all tables
+    const [tables, setTables] = useState()
+
+    // state for the actions and type of category
+    const [action, setAction] = useState()
+    const [category, setCategory] = useState()
+
+
 
     useEffect(() => {
-        console.log(action)
-    })
+        const axiosData = async url => {
+            const res = await axios.get(url)
+            setTables(res.data)
+        }
+        axiosData('http://localhost:4000/admin/')
+    }, [])
 
     return (
-        <form>
-            <select onChange={(e) => setAction(e.target.value)}>
-                <option value="default">Actions</option>
-                <option value="creer">Créer</option>
-                <option value="modifier">Modifier/Supprimer</option>
-                <option value="afficher">Afficher</option>
-            </select>
-            <select>
-            
-            </select>
-        </form>
+        <>
+            <form>
+                <select onChange={(e) => setAction(e.target.value)}>
+                    <option value="">Actions</option>
+                    <option value="créer">Créer</option>
+                    <option value="modifier">Modifier/Supprimer</option>
+                    <option value="afficher">Afficher</option>
+                </select>
+                {tables ?
+                    <select onChange={(e) => setCategory(e.target.value)}>
+                        <option value="">Catégorie</option>
+                        {tables.map(item => (
+                            <option value={item.table_name}>{item.table_name}</option>))}
+                    </select> : console.log("stop")}
+                <input type="button" value="Valider" />
+            </form>
+            {action && tables ?
+                <ArticleInform  action={action} category={category}/> : console.log("nothing has been chosen")
+            }
+        </>
 
     )
 }
