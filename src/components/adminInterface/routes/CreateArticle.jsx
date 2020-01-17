07770652039c4578747c87e47860c7e32dbed57e
Pauline Drouhin <pauline.drouhin@gmail.com>
2020-01-17 05:04:15 +0100

    article object to pass to back-end with proper key/values

--- src/components/adminInterface/routes/CreateArticle.jsx
+++ src/components/adminInterface/routes/CreateArticle.jsx
@@ -6,6 +6,7 @@ import { Editor } from '@tinymce/tinymce-react';
 
 export default function CreateArticle(props) {
   // les states
+  ///// article /////
   const [title, setTitle] = useState()
   const [author, setAuthor] = useState()
   const [date, setDate] = useState()
@@ -15,9 +16,16 @@ export default function CreateArticle(props) {
   const [text, setText] = useState()
   const [isPublished, setPublished] = useState()
   const [titleList, setTitleList] = useState()
+  ///// table intermédiaire ////
+  const [initiatives, setInitiatives] = useState([])
+  const [besoins, setBesoins] = useState([])
+  const [types_activites, setTypes_activites] = useState([])
+  const [categories_objets, setCategories_objets] = useState([])
+  const [objets, setObjets] = useState([])
+
 
   // MEGA STATE!!
-  const articleData = { title, author, date, img, readingTime, place, text, isPublished, titleList }
+  const articleData = { titre: title, auteur: author, date: date, image: img, minutes_lecture: readingTime, geographie: place, contenu: text, publication: isPublished, listes_initiatives: titleList }
 
   useEffect(() => {
     console.log(text)
@@ -36,13 +44,13 @@ export default function CreateArticle(props) {
       .then(res => res.json())
     e.preventDefault()
   }
-
+  console.log(articleData)
   return (
     <div>
       <h1>Je crée un article</h1>
       <form className="form" id="form1">
         <p>
-          <input type="text" value="" className="feedback-input" placeholder="Titre" id="titre" onChange={(e) => setTitle(e.target.value)} />
+          <input type="text" className="feedback-input" id="titre" placeholder="titre" onChange={(e) => setTitle(e.target.value)} />
         </p>
         <p>
           <input type="text" className="feedback-input" id="auteur" placeholder="Auteur" onChange={(e) => setAuthor(e.target.value)} />
@@ -77,21 +85,24 @@ export default function CreateArticle(props) {
         }}
         onChange={(e) => setText(e.target.getContent())}
       />
-       
-      {/* la liste de bidules à associer au à l'article */}
-        <div className="association">
-          <h3>J'associe mon article à des objets et des besoins</h3>
-          <div className="listeAssociation"></div>
-          <label className="listName"> Titre: 
-          <input type='text' value={titleList} onChange={(e) => setTitleList(e.target.value)}/>
-          </label>
-          <h4>Je peux ajouter initiatives à mon article</h4>
-          <div className="initiatives"></div>
-          <div className="publication"> Mon article est publié
-          <input type="checkbox" onChange={() => setPublished(!isPublished)}></input>
-           </div>
-        </div>
 
+      {/* la liste de bidules à associer au à l'article */}
+      <div className="association">
+        <h3>J'associe mon article à des objets et des besoins</h3>
+        <div className="listeAssociation"></div>
+        <label className="listName"> Titre:
+          <input type='text' value={titleList} onChange={(e) => setTitleList(e.target.value)} />
+        </label>
+        <h4>Je peux ajouter initiatives à mon article</h4>
+        <div className="initiatives"></div>
+        <div className="publication"> Mon article est publié
+          <input type="checkbox" onChange={() => setPublished(!isPublished)}></input>
+        </div>
+      </div>
+
+{/* 1) rajouter les filtres et les stocker dans le state de façon à les faire passer de façon intelligible par le bac
+
+rajouter la liste des initiatives liées grâce à un axios qd on valide les filtres au dessus*/}
       <input type="button" value="SEND" id="button-blue" onClick={(e) => handlePost(e)} />
     </div>
   )
