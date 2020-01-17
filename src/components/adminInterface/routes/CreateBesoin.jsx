ef9ae2763544d008fa4def42f1350a88284fec59
Tass2T <brunog90@gmail.com>
2020-01-17 03:31:47 +0100

    about to create a new branch for display

--- src/components/adminInterface/routes/CreateBesoin.jsx
+++ src/components/adminInterface/routes/CreateBesoin.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function CreateBesoin () {
-    return(
+export default function CreateBesoin() {
+
+    const [labelName, setLabelName]=useState()
+    const [picto, setPicto]=useState()
+    const newBesoin = {labelName, picto}
+
+    return (
         <div className="creation-besoin">
-        <h3>J'ajoute un besoin</h3>
+            <h3>J'ajoute un besoin</h3>
+            <div>
+                <label>Nom du label: <input type="text" onChange={(e) => setLabelName(e.target.value)}/></label>
+                <label>Pictogramme: <input type="url" onChange={(e) => setPicto(e.target.value)}/></label>
+            </div>
+            <input type="button" onClick=""/>
+            <h3>J'ajoute un type d'activité: </h3>
+
         </div>
 
     )
