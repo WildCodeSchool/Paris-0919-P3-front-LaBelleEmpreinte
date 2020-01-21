7ec24ddbe6be71229f0eb4f477cbcfa17e74bff8
Tass2T <brunog90@gmail.com>
2020-01-21 15:15:01 +0100

    looking for missing documents

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 
-import React, {useState} from 'react';
-import Header from './components/screen/Header';
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
-import Navbar from './components/screen/Navbar';
 import Footer from './components/screen/Footer';
+import GetHeaderHeight from './components/screen/GetHeaderHeight';
 import Filtres from './components/userInterface/Filtres';
+import Home from './components/adminInterface/Home'
 
 const App = () => {
   const [headerHeight, setHeaderHeight] = useState(0)
 
   const setRealHeaderHeight = (height) => {
-    
+
     setHeaderHeight({ headerHeight: height })
   }
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <div className="app_container">
- 
-      <Header/>
-      <div className="categorie">
-      <Filtres />
-      
-      </div>
-      <Footer />
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <div className="app_container">
+            <GetHeaderHeight props={setRealHeaderHeight} />
+            <div className="categorie">
+              <Filtres />
+            </div>
+            <Footer />
+          </div>
+        </Route>
+        <Route path="/admin">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
