dc532d50b8980e6a017f55b631607bcd54c69c97
Tass2T <brunog90@gmail.com>
2020-02-05 16:03:08 +0100

    done

--- src/components/adminInterface/routes/ModifyFilters/ModifyCategoriesObjets.js
+++ src/components/adminInterface/routes/ModifyFilters/ModifyCategoriesObjets.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+// on importe le menu modal
+import { Modal, ModalBody, ModalHeader } from 'reactstrap'
 
 export default function ModifyCategory(props) {
 
@@ -16,6 +18,33 @@ export default function ModifyCategory(props) {
     const [categorie, setCategorie] = useState()
     const [picto, setPicto] = useState()
     const newCat = { categorie, picto, id }
+
+    // rend visible/invisible le menu modal
+    const [visible, setVisible] = useState(false)
+    const [modifVisible, setModifVisible] = useState(false)
+    // reçoit la réponse depuis le back
+    const [theRes, setRes] = useState()
+
+    //Pour les modals
+    const handleClickModif = () => {
+        setModifVisible(!modifVisible)
+
+    }
+
+    const handleClick = () => {
+        setVisible(!visible)
+    }
+
+    //affiche la confirmation de modification
+    useEffect(() => {
+        if (theRes) {
+
+            if (theRes.statusText === "OK") {
+                setModifVisible(true)
+            }
+        }
+    }, [theRes]
+    )
 
     const loadStates = () => {
         setCategorie(newElem.categorie)
@@ -37,6 +66,7 @@ export default function ModifyCategory(props) {
     const handlePost = () => {
         const url = 'http://localhost:4000/admin/categories_objets/modify'
         axios.put(url, newCat)
+            .then(res => setRes(res))
     }
 
     const deletePost = () => {
@@ -45,42 +75,60 @@ export default function ModifyCategory(props) {
     }
 
     return (
+        <>
+            {/* MODAL DE MODIFICATION */}
+            < Modal isOpen={modifVisible} toggle={handleClickModif} className="" >
+                <ModalHeader toggle={handleClickModif}>La catégorie d'objet a bien été modifiée</ModalHeader>
+                <ModalBody>
+                    <div className="menu-modal">
+                        <Link to="/admin/afficher/categories_objets"><input type="button" value="OK" /></Link>
+                    </div>
+                </ModalBody>
+            </Modal >
 
-        <div className="admincreatearticle">
-            <h1>Je modifie une catégorie d'objet</h1>
-            <div id="form-main">
-                <div id="form-div">
-                    <label>Nom de la catégorie d'objet: <input className="feedback-input" type="text" value={categorie} onChange={(e) => setCategorie(e.target.value)} /></label>
-                    <p>
-                        Pictogramme
+            {/* MODAL DE SUPPRESSION */}
+            < Modal isOpen={visible} toggle={handleClick} className="" >
+                <ModalHeader toggle={handleClick}>Souhaitez vraiment supprimer la catégorie d'objet?</ModalHeader>
+                <ModalBody>
+                    <div className="menu-modal">
+                        <Link to="/admin/afficher/categories_objets"><input type="button" value="Confirmer" onClick={() => deletePost()} /></Link>
+                    </div>
+                </ModalBody>
+            </Modal >
+
+            <div className="admincreatearticle">
+                <h1>Je modifie une catégorie d'objet</h1>
+                <div id="form-main">
+                    <div id="form-div">
+                        <label>Nom de la catégorie d'objet: <input className="feedback-input" type="text" value={categorie} onChange={(e) => setCategorie(e.target.value)} /></label>
+                        <p>
+                            Pictogramme
                 <input
-                            type="text"
-                            className="feedback-input"
-                            id="image"
-                            value={picto}
-                            placeholder="Image(url)"
-                            onChange={(e) => setPicto(e.target.value)} />
-                    </p>
-                    <div classname="button">
-                        <Link to="/admin/afficher/categories_objets">
-                            <input
+                                type="text"
                                 className="feedback-input"
-                                type="button"
-                                value="Modifier objet" id="button-blue"
-                                onClick={e => handlePost(e)}
-                            />
-                            <input
-                                className="feedback-input"
-                                type="button"
-                                value="Supprimer objet" id="button-blue"
-                                onClick={() => deletePost()}
-                            />
-                        </Link>
+                                id="image"
+                                value={picto}
+                                placeholder="Image(url)"
+                                onChange={(e) => setPicto(e.target.value)} />
+                        </p>
+                        <div classname="button">
+                                <input
+                                    className="feedback-input"
+                                    type="button"
+                                    value="Modifier objet" id="button-blue"
+                                    onClick={e => handlePost(e)}
+                                />
+                                <input
+                                    className="feedback-input"
+                                    type="button"
+                                    value="Supprimer objet" id="button-blue"
+                                    onClick={() => handleClick()}
+                                />
+                        </div>
                     </div>
                 </div>
+
             </div>
-
-        </div>
-
+        </>
     )
 }
--- src/components/adminInterface/routes/ModifyFilters/ModifyTypeActivity.js
+++ src/components/adminInterface/routes/ModifyFilters/ModifyTypeActivity.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-
-import '../../CSS/CreateObjets.css'
-
 import { Link } from 'react-router-dom'
+import '../../CSS/CreateObjets.css'
+// on importe le menu modal
+import { Modal, ModalBody, ModalHeader } from 'reactstrap'
 
 
 export default function ModifyTypeActivity(props) {
@@ -19,90 +19,130 @@ export default function ModifyTypeActivity(props) {
     const [picto, setPicto] = useState()
     const [filtres, setFiltres] = useState([])
     const [besoins_id, setBesoins_id] = useState()
-    const [id, setId] = useState ()
+    const [id, setId] = useState()
 
     const newCat = { types_activites, picto, besoins_id, id }
 
+    // rend visible/invisible le menu modal
+    const [visible, setVisible] = useState(false)
+    const [modifVisible, setModifVisible] = useState(false)
+    // reçoit la réponse depuis le back
+    const [theRes, setRes] = useState()
+
+    //Pour les modals
+    const handleClickModif = () => {
+        setModifVisible(!modifVisible)
+    }
+
+    const handleClick = () => {
+        setVisible(!visible)
+    }
+
+    //affiche la confirmation de modification
+    useEffect(() => {
+        if (theRes) {
+
+            if (theRes.statusText === "OK") {
+                setModifVisible(true)
+            }
+        }
+    }, [theRes]
+    )
     const loadStates = () => {
         setTypes_activites(newElem.types_activites)
         setPicto(newElem.picto)
         setId(newElem.id)
         setBesoins_id(newElem.besoins_id)
         setChange(false)
-      }
+    }
 
     useEffect(() => {
         setElem(props)
         setNewElem(Elem.elem)
-        if (newElem &&justChange){
-          loadStates()
+        if (newElem && justChange) {
+            loadStates()
         }
-      })
+    })
 
     // a checker le chemin ==> nouveau besoin
     const handlePost = () => {
         const url = 'http://localhost:4000/admin/types_activites/modify'
-           axios.put(url, newCat)
-        }
+        axios.put(url, newCat)
+            .then(res => setRes(res))
+    }
 
-        const deletePost = () => {
-            const url = `http://localhost:4000/admin/types_activites/${id}`
-            axios.delete(url)
-        }
+    const deletePost = () => {
+        const url = `http://localhost:4000/admin/types_activites/${id}`
+        axios.delete(url)
+    }
 
-        useEffect(() => {
-            const getFilters = () => {
-                axios
+    useEffect(() => {
+        const getFilters = () => {
+            axios
                 .get("http://localhost:4000/user/besoins")
                 .then(response => response.data)
                 .then(data => {
-                  setFiltres(data[0].results);
+                    setFiltres(data[0].results);
                 });
-                
-            }
-            getFilters()
-        }, [])
+
+        }
+        getFilters()
+    }, [])
 
     return (
+        <>
+            {/* MODAL DE MODIFICATION */}
+            < Modal isOpen={modifVisible} toggle={handleClickModif} className="" >
+                <ModalHeader toggle={handleClickModif}>Le type d'activité a bien été modifié</ModalHeader>
+                <ModalBody>
+                    <div className="menu-modal">
+                        <Link to="/admin/afficher/types_activites"><input type="button" value="OK" /></Link>
+                    </div>
+                </ModalBody>
+            </Modal >
 
-        <div className="admincreatearticle">
-            <h1>Je modifie un type d'activité</h1>
-            <div id="form-main">
-                <div id="form-div">
-                <form className="form" id="form1"> 
-                <label>Nom du type d'activité : <input className="feedback-input" type="text" onChange={(e) => setTypes_activites(e.target.value)} /></label>
-                <p>
-                Pictogramme
+            {/* MODAL DE SUPPRESSION */}
+            < Modal isOpen={visible} toggle={handleClick} className="" >
+                <ModalHeader toggle={handleClick}>Souhaitez vraiment supprimer le type d'activité?</ModalHeader>
+                <ModalBody>
+                    <div className="menu-modal">
+                        <Link to="/admin/afficher/types_activites"><input type="button" value="Confirmer" onClick={() => deletePost()} /></Link>
+                    </div>
+                </ModalBody>
+            </Modal >
+
+            <div className="admincreatearticle">
+                <h1>Je modifie un type d'activité</h1>
+                <div id="form-main">
+                    <div id="form-div">
+                        <form className="form" id="form1">
+                            <label>Nom du type d'activité : <input className="feedback-input" type="text" onChange={(e) => setTypes_activites(e.target.value)} /></label>
+                            <p>
+                                Pictogramme
                 <input
-                  type="text"
-                  className="feedback-input"
-                  id="image"
-                  value={picto}
-                  placeholder="Image(url)"
-                  onChange={(e) => setPicto(e.target.value)}/>
-              </p> 
+                                    type="text"
+                                    className="feedback-input"
+                                    id="image"
+                                    value={picto}
+                                    placeholder="Image(url)"
+                                    onChange={(e) => setPicto(e.target.value)} />
+                            </p>
 
 
-               <label> Catégorie intermédiaire associée <select onChange={(e) => setBesoins_id(e.target.value)}> 
-                    {filtres.map(filtre => {
-                       return <option value={filtre.id}> {filtre.besoins}</option>
-                    })
-                }
-                </select></label>            
+                            <label> Catégorie intermédiaire associée <select onChange={(e) => setBesoins_id(e.target.value)}>
+                                {filtres.map(filtre => {
+                                    return <option value={filtre.id}> {filtre.besoins}</option>
+                                })
+                                }
+                            </select></label>
+                                <input type="button" id="button-blue" className="feedback-input" value="Modifier le type d'activité" onClick={() => handlePost()} />
+                                <input type="button" id="button-blue" className="feedback-input" value="Supprimer le type d'activité" onClick={() => handleClick()} />
 
-                <Link to="/admin/afficher/types_activites">
-            <input type="button" id="button-blue" className="feedback-input" value="Modifier le type d'activité" onClick={handlePost} />
-            <input type="button" id="button-blue" className="feedback-input" value="Supprimer le type d'activité" onClick={() => deletePost()} />
-            </Link>  
-
-            </form>
+                        </form>
+                    </div>
+                </div>
             </div>
-            </div>
-
-            
-
-        </div>
-        
+        </>
 
     )
 }
